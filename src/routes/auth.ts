9978461b17e63f74d2315e665abd0a1cce8f1f98
ugwu stanley <56942462+ugwustanley@users.noreply.github.com>
2021-06-08 07:36:02 +0100

    changed post to put

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,7 +6,7 @@ const route = Router();
 
 route.get("/user", AuthController.get_user_by_email);
 
-route.post("/isverified", AuthController.set_verified);
+route.put("/isverified", AuthController.set_verified);
 
 route.get("/users", AuthController.list_of_users);
 
